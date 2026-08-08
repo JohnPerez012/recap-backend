b75f8c6353e6f01d24392a87b8df83c53d3474ea
JOHNpCADAROO <126931681+JohnPerez012@users.noreply.github.com>
2026-08-09 01:20:36 +0800

    Delete public directory

--- public/js/view_project_details.js
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. The project details view has been fused into index.html as a single-page application view.
-// The rendering logic is now defined inline in index.html and managed via ViewManager.
-// The DOMContentLoaded redirect logic was removed to prevent infinite redirect loops.
-
