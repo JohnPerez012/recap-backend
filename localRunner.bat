0b9b81eca0cb4230795483a0e5de4cb3e225f9aa
JOHNpCADAROO <126931681+JohnPerez012@users.noreply.github.com>
2026-08-09 01:21:41 +0800

    Delete localRunner.bat

--- localRunner.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-wt ^
-new-tab --title "Back (3001)" cmd /k "cd/d Desktop\RE-CAPS\recap-backend && npm start" ^
-; new-tab --title "Front (5000)" cmd /k "cd/d Desktop\RE-CAPS && npx serve public"
